7c0afb1a25476f6854ead9a8a1297296e489b443
Denis Z <d10nZ@users.noreply.github.com>
2022-09-27 18:25:10 +0400

    Initial commit

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © 2022 Studio Raz. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'SR_{ModuleName}',
+    __DIR__
+);
